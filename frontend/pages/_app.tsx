d1f2eb4f6fa0e56a8656b32e78aed8817ef21ffa
graxxia <60703352+graxxia@users.noreply.github.com>
2023-05-09 13:46:22 -0400

    Dev (#15)
    * Added Google Analytics
    
    
    remove local secrets
    
    * Fixed Key
    
    * Added git secret

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,58 @@
 import "../styles/globals.css";
 import "../components/nav";
-import { SWRConfig } from "swr";
-import fetchJson from "lib/fetchJson";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../util/gtag";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  const googleAnalyticsKey = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
   return (
     <>
       <Head>
         <title>Gracia Morales</title>
+        <meta
+          name="description"
+          content="Gracia Morales Portfolio website, graphic designer and webdesigner"
+        ></meta>
+        <meta
+          name="keywords"
+          content="Graphic Designer, 3D, Logo Design, Web Design"
+        ></meta>
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsKey}`}
+      />
+      <Script
+        id="google"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsKey}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <Component {...pageProps} />
     </>
